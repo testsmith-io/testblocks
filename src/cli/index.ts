a86afbd2f22a5da42669a4e557b3b0bfdf5eb89b
Roy de Kleijn <dekleijn.roy@gmail.com>
2026-01-03 09:54:57 +0100

    renamed to @testsmith/testblocks

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -224,7 +224,7 @@ program
           'test:junit': 'testblocks run tests/**/*.testblocks.json -r junit -o reports',
         },
         devDependencies: {
-          testblocks: '^0.6.0',
+            '@testsmith/testblocks': '^0.1.0',
         },
       };
       fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
